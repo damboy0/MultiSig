a890cf90d05f4f49017bb75eee322b7fdd587e25
damboy0 <fagbolade006@gmail.com>
2024-09-12 08:12:14 +0100

    add Interaction Script

--- scripts/Interaction.ts
+++ scripts/Interaction.ts
@@ -1,2 +1,21 @@
 // MultisigModule#Multisig - 0x5FbDB2315678afecb367f032d93F642f64180aa3
 
+import { ethers } from "hardhat";
+
+
+async function main(){
+    const web3CXITokenAddress = "";
+    const web3CXI = await ethers.getContractAt("IERC20", web3CXITokenAddress);
+
+
+    const multisigContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    const multisig = await ethers.getContractAt("Multisig",multisigContractAddress)
+
+    
+
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+   });
